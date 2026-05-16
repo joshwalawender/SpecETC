65f09b06a71db25a592f3e8e45514147033ff0fc
Josh Walawender <joshwalawender@me.com>
2026-05-15 15:12:58 -1000

    Get Slit Throughput calculation working

---
+++ slitloss/slitloss.py
@@ -0,0 +1,58 @@
+import numpy as np
+from astropy.modeling.models import Gaussian2D, Moffat2D
+from astropy import units as u
+
+
+def slit_throughput(seeing, slit_width,
+                    amplitude=1, alpha=1,
+                    sample_size=0.02, # arcsec
+                   ):
+    # Generate PSF
+    if isinstance(seeing, u.Quantity): seeing = seeing.to(u.arcsec).value
+    psf = Moffat2D(amplitude=amplitude, x_0=0, y_0=0,
+                   gamma=seeing/2, alpha=alpha)
+#     psf = Gaussian2D(amplitude=amplitude, x_mean=0, y_mean=0,
+#                      x_stddev=seeing/2.355, y_stddev=seeing/2.355)
+    gstart = -3*seeing # arcsec
+    gend = -gstart+sample_size # arcsec
+    gx = np.arange(gstart, gend, sample_size)
+    gy = np.arange(gstart, gend, sample_size)
+    xv, yv = np.meshgrid(gx, gy)
+    total_psf_flux = np.sum(psf(xv, yv))
+
+    # Generate Slit Mask
+    if not isinstance(slit_width, u.Quantity): slit_width *= u.arcsec
+    slit_width = slit_width.to(u.arcsec).value
+    slit_start = int(np.argmin(abs(gx+slit_width/2)))
+    slit_end = int(np.argmin(abs(gx-slit_width/2)))
+    w = (xv > gx[slit_start]) & (xv < gx[slit_end])
+    wint = np.array(w, dtype=int)
+
+    slit_flux = float(np.sum(psf(xv[w], yv[w])))
+    slit_throughput = float(slit_flux/total_psf_flux)
+
+    return slit_throughput
+
+
+
+
+#     if plot:
+#         plt.subplot(2,Nt,i+1)
+#         title = f"{tel.name}\n"
+#         title += f"{seeing:.1f}'' Seeing, {slit_width:.2f}'' slit\n"
+#         title += f"Eff. = {slit_throughput:.1%} "
+#         title += f"Flux = {slit_throughput*tel.area:.0f}"
+#         plt.title(title, size=10)
+#         plt.imshow(psf(xv, yv)*wint, cmap='Greys', origin='lower', vmin=0, vmax=1e-16)
+#         plt.gca().set_xticks([])
+#         plt.gca().set_yticks([])
+#         plt.axvline(slit_start, color='k')
+#         plt.axvline(slit_end, color='k')
+# 
+#         plt.subplot(2,Nt,i+1+Nt)
+#         plt.imshow(psf(xv, yv)*(1-wint), cmap='Reds', origin='lower', vmin=0, vmax=1e-16)
+#         plt.gca().set_xticks([])
+#         plt.gca().set_yticks([])
+#         plt.axvline(slit_start, color='k')
+#         plt.axvline(slit_end, color='k')
+# if plot: plt.show()
---
+++ telescope/telescope.py
@@ -0,0 +1,28 @@
+import numpy as np
+from astropy import units as u
+
+
+class Telescope(object):
+    def __init__(self, name, aperture, fl, obstruction=0):
+        self.name = name
+        self.aperture = aperture
+        if not isinstance(self.aperture, u.Quantity): self.aperture *= u.mm
+        self.fl = fl
+        if not isinstance(self.fl, u.Quantity): self.fl *= u.mm
+        self.fratio = self.fl.to(u.mm)/self.aperture.to(u.mm)
+        # ALPY 600 Spectrograph only accepts light from f/4 beam or slower
+        if self.fratio < 4:
+            self.aperture = self.fl/4
+        self.obstruction = obstruction
+        if not isinstance(self.obstruction, u.Quantity): self.obstruction *= u.mm
+        self.area = np.pi*(self.aperture**2-self.obstruction**2).to(u.cm**2)
+
+    def slit_width(self, slit_size):
+        if not isinstance(slit_size, u.Quantity): slit_size *= u.micron
+        return 206265*u.arcsec*slit_size.to(u.mm)/self.fl.to(u.mm)
+
+    def __str__(self):
+        return f"{self.name}"
+
+    def __repr__(self):
+        return f"{self.name} (EffD={self.aperture:.0f}mm, FL={self.fl:.0f}mm)"
